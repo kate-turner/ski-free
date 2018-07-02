25378777c5821df4cedb62a8acdd36d6a280bbbc
Kathleen Turner <kathleenturner@Kathleens-MacBook-Pro.local>
2018-07-01 21:57:02 -0600

    skier moves l/r

--- js/app.js
+++ js/app.js
@@ -1,19 +1,66 @@
-console.log("up");
-
-// const skier =
 
 
-const gameBoard = [ [0,0,0,0,0],
-					[0,0,0,0,0],
-					[0,0,0,0,0],
-					[0,0,0,0,0],
+const skier = {
+	lives: 3,
+	xCoordinate: 5,
+	moveLeft(){
+		if(this.xCoordinate > 0){
+			console.log("ROOM TO MOVE LEFT")
+			$(`.game-square-${this.xCoordinate}-0`).removeClass('skier');
+			this.xCoordinate -= 1;
+			$(`.game-square-${this.xCoordinate}-0`).addClass('skier')
+		}
+	},
+	moveRight(){
+		if(this.xCoordinate < 10){
+			console.log("ROOM TO MOVE RIGHT")
+			$(`.game-square-${this.xCoordinate}-0`).removeClass('skier');
+			this.xCoordinate += 1;
+			$(`.game-square-${this.xCoordinate}-0`).addClass('skier')
+		}
+	}
+}
+// const obstacles = [],
+// const obstacleNumber = 1;
+class Obstacle {
+	constructor(xCoordinate){
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = 9;
+	}
+}
+
+
+
+const gameBoard = [ [0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0]
+
 ]
 
 for(let i = 0; i < gameBoard.length; i++) {
 	let row = gameBoard[i];
-	$('.game-board').append(`<div class='game-row'></div>`)
-	for(let x = 0; i < row.length; x++) {
-		$('.game-row').append('<div class="game-square"></div>')
+	$('.game-board').append(`<div class='game-row-${i} game-row'></div>`)
+	for(let x = 0; x < row.length; x++) {
+		$(`.game-row-${i}`).append(`<div class="game-square game-square-${x}-${i}"></div>`)
 		}
-		
-};
+}
+$('.game-square-5-0').addClass('skier');
+$(document).keydown(function(e){
+	let keyPressed = e.which;
+	if(keyPressed == 37){
+		skier.moveLeft();
+	}	else if(keyPressed == 39){
+		skier.moveRight();
+	}	
+})
+
+
+
+$('.game-square-2-9').addClass('Obstacle');
